85cfcd97b5a5c3e994c83748adfe54216e0f45e8
CristianRG <cristianrizogomez890@gmail.com>
2025-03-18 23:36:27 -0600

    refactor: extend Physics class from PhysicsInjector for improved integration

--- src/core/models/Physics.ts
+++ src/core/models/Physics.ts
@@ -1,4 +1,6 @@
-export abstract class Physics {
+import { PhysicsInjector } from "./Injector";
+
+export abstract class Physics extends PhysicsInjector {
     public apply: boolean = true;
     public type: "physics" | "gravity" | "jump" = "physics";
     public abstract applyPhysics(): void;
