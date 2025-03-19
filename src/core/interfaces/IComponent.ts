56716c9ecbef869523d0a4d298d82d595004d963
CristianRG <cristianrizogomez890@gmail.com>
2025-03-18 22:06:04 -0600

    add: include _components property in IComponentMethods interface

--- src/core/interfaces/IComponent.ts
+++ src/core/interfaces/IComponent.ts
@@ -1,6 +1,7 @@
 export interface IComponent { }
 
 export interface IComponentMehtods {
+    _components: Map<string, IComponent>;
     addComponent(component: IComponent): this;
     getComponent<T extends IComponent>(type: new () => T): T | undefined;
     hasComponent<T extends IComponent>(type: new () => T): boolean;
