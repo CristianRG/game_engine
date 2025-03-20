fd780c461184fa73b6f40b2b128e0ffa265d2cef
CristianRG <cristianrizogomez890@gmail.com>
2025-03-20 17:36:16 -0600

    refactor: remove IEntity and IGameObject references from IScene interface

--- src/core/interfaces/IScene.ts
+++ src/core/interfaces/IScene.ts
@@ -1,10 +1,5 @@
-import { IEntity } from "./IEntity";
-import { IGameObject } from "./IGameObject";
-
 export interface IScene {
     id: number;
-    entities: IEntity[];
-    objects: IGameObject[];
 }
 
 export interface IRenderStrategy {
