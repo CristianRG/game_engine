69d2bb1808631282ce524bbd45c94eb041df95cb
CristianRG <cristianrizogomez890@gmail.com>
2025-03-21 19:03:18 -0600

    refactor: update Scene class to extend ComponentManager and implement IGameObject interface

--- src/core/models/Scene.ts
+++ src/core/models/Scene.ts
@@ -1,15 +1,16 @@
 import { SceneDecorator } from "../decorators/SceneDecorator";
 import { IGameObject } from "../interfaces/IGameObject";
-import { IScene } from "../interfaces/IScene";
-import { GameObject } from "./GameObject";
 import { ComponentManager } from "./Injector";
 
 @SceneDecorator.registerScene
-export class Scene extends GameObject {
+export class Scene extends ComponentManager implements IGameObject {
+    private static _id: number = 0;
+    id: number;
     canvas: HTMLCanvasElement;
 
     constructor(canvas: HTMLCanvasElement) {
         super();
+        this.id = Scene._id++;
         this.canvas = canvas;
     }
 }
