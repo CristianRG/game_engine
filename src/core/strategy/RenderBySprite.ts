dcf880f6aa25ee0925ddd88d9637c044483d186b
CristianRG <cristianrizogomez890@gmail.com>
2025-03-22 19:15:23 -0600

    refactor: update rendering strategies to utilize Entities and GameObjects for improved scene management

--- src/core/strategy/RenderBySprite.ts
+++ src/core/strategy/RenderBySprite.ts
@@ -2,27 +2,47 @@ import { IRenderEntityStrategy } from "../interfaces/IStrategy";
 import { Sprite } from "../../components/Sprite";
 import { SpriteAnimationController } from "../models/SpriteMethods";
 import { GlobalState } from "../state/GlobalState";
+import { Scene } from "../models/Scene";
+import { Entity } from "../models/Entity";
+import { GameObject } from "../models/GameObject";
+import { Entities } from "../../components/scene/Entities";
+import { GameObjects } from "../../components/scene/GameObjects";
+import { SpriteComponent } from "../..";
 
 export class RenderBySprite extends SpriteAnimationController implements IRenderEntityStrategy {
-    sprites: Sprite[] = [];
+    private scene: Scene;
+    private entities: Entity[] = [];
+    private gameObjects: GameObject[] = [];
     ctx: CanvasRenderingContext2D | null = null;
     stopped: boolean = false;
 
-    constructor(ctx: CanvasRenderingContext2D, sprites: Sprite[]) {
+    constructor(scene: Scene) {
         super();
-        this.ctx = ctx;
-        //this.sprites = sprites;
+        this.scene = scene;
+        this.ctx = scene.canvas.getContext("2d") as CanvasRenderingContext2D;
     }
 
     render(): void {
+        const sprites: SpriteComponent[] = []
         if (this.stopped || !this.ctx) return;
-        const scene = GlobalState.getInstance().scenes[0];
 
-        this.sprites = [
-            ...scene.entities.filter(e => e.hasComponent(Sprite)).map(e => e.getComponent(Sprite)!),
-            ...scene.objects.filter(e => e.hasComponent(Sprite)).map(e => e.getComponent(Sprite)!)
-        ];
+        if (this.scene.hasComponent(SpriteComponent)) {
+            sprites.push(this.scene.getComponent(SpriteComponent) as SpriteComponent);
+        }
 
-        this.play(this.ctx, this.sprites);
+        if (this.scene.hasComponent(Entities)) {
+            this.entities = this.scene.getComponent(Entities)!.getEntities();
+        }
+
+        if (this.scene.hasComponent(GameObjects)) {
+            this.gameObjects = this.scene.getComponent(GameObjects)!.getObjects();
+        }
+
+        sprites.push(
+            ...this.entities.filter(e => e.hasComponent(SpriteComponent)).map(e => e.getComponent(SpriteComponent) as SpriteComponent),
+            ...this.gameObjects.filter(o => o.hasComponent(SpriteComponent)).map(o => o.getComponent(SpriteComponent) as SpriteComponent)
+        );
+
+        this.play(this.ctx, sprites);
     }
 }
