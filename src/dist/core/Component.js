8c23ccb97c7fe47beec61748b86d6588137cfe6e
CristianRG <cristianrizogomez890@gmail.com>
2025-03-27 23:11:32 -0600

    update: removed unused files

--- src/dist/core/Component.js
+++
@@ -1,5 +0,0 @@
-/**
- * Base class for all components in the engine. Components are used to add functionality to entities.
- */
-export class Component {
-}
--- src/dist/core/System.js
+++
@@ -1,5 +0,0 @@
-/**
- * System class. Represents a system in the game. It is responsible for add functionality to entities.
- */
-export class System {
-}
